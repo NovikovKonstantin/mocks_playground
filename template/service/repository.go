f0a071952e772a71f22061049fc1f50dea89309d
NovikovKonstantin <novikov.konstantin.nk@gmail.com>
2023-05-13 17:13:09 +0300

    Update template

--- template/service/repository.go
+++ template/service/repository.go
@@ -1,7 +1,6 @@
 package service
 
 type Repository interface {
-	Store(value int64) (string, error)
-	GetByKey(key string) (int64, error)
-	GetList() ([]int64, error)
+	Store(values []int64) ([]string, error)
+	Get(keys []string) ([]int64, error)
 }
